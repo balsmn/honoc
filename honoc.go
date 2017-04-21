d95c02d998af0b10fb66c0069ca5394eb0141f17
balsmn <balsmn@gmail.com>
2017-04-21 20:45:39 +0200

    added concurrent telemetry from hono clients capability for the CLI
    Signed-off-by: balsmn <balsmn@gmail.com>

--- honoc.go
+++ honoc.go
@@ -5,22 +5,60 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 )
+
+type TelemetryControl struct {
+	telemetry bool //indicates whether telemetry data should be sent
+	noDelay   bool //whether telemetry data should be sent without any delay
+	protocol  PROTOCOL
+}
 
 //checks the user provided device id and returns the same. If none,
 //generates a new one and returns the same
 func GetDeviceId(deviceId int) int {
 	if deviceId == 0 {
-		return rand.Intn(time.Now().Nanosecond())
+		return GetRandomDeviceId()
 	} else {
 		return deviceId
 	}
 }
 
+//generates a random device id
+func GetRandomDeviceId() int {
+	return rand.Intn(time.Now().Nanosecond())
+}
+
+//generate random temperature value
 func GetRandomTemperature() string {
-	//random temperature value
 	return fmt.Sprintf("{\"temp\": %d}", rand.Int31n(100))
+}
+
+//registers a device and sends telemetry if registration was successful
+func RegisterAndSendTelemetry(honoClient HonoClient, tenant string, deviceId int, tc TelemetryControl) {
+	resp, err := CreateDevice(honoClient, tenant, deviceId)
+
+	if err == nil && resp.StatusCode != http.StatusNotFound {
+		GetDeviceAndSendTelemetry(honoClient, tenant, deviceId, tc)
+	}
+}
+
+//gets the device from Hono, if the device is active, sends telemetry data
+func GetDeviceAndSendTelemetry(honoClient HonoClient, tenant string, deviceId int, tc TelemetryControl) {
+	//get the registered device
+	device, _, _ := GetDevice(honoClient, tenant, deviceId)
+
+	if tc.telemetry && device.DATA.ENABLED {
+		//send telemetry data
+		resp, err := SendTelemetry(honoClient, tenant, deviceId, GetRandomTemperature())
+		for err == nil && resp.StatusCode == http.StatusAccepted {
+			if tc.noDelay != true {
+				time.Sleep(time.Second * 1)
+			}
+			resp, err = SendTelemetry(honoClient, tenant, deviceId, GetRandomTemperature())
+		}
+	}
 }
 
 func main() {
@@ -30,40 +68,36 @@ func main() {
 	tenant := flag.String("t", "DEFAULT_TENANT", "default value is DEFAULT_TENANT")
 	inputDeviceId := flag.Int("d", 0, "test device id. Leave it to default 0 to generate a random device id")
 	register := flag.Bool("r", false, "set to true to register a new device, default is false")
+	noOfClients := flag.Int("n", 1, "number of clients. This value takes effect only used in conjunction with -r. When the value is set to more than 1, then the input to -d is simply ignored")
 	telemetry := flag.Bool("tm", false, "sends random telemetry data when set to true, default is false")
 	noDelay := flag.Bool("c", false, "sends telemetry continously without any delay, default is false")
+	p := flag.Int("tp", 1, "protocol for sending telemetry data [1 for http| 2 for mqtt| 3 for amqp]")
 
 	flag.Parse()
 
-	httpClient := http.DefaultClient
-	honoClient := NewHonoRestClient(httpClient, *url)
-	var device = new(DEVICE)
-	deviceId := GetDeviceId(*inputDeviceId)
-
-	//Register a new device
-	if *register {
-		resp, err := CreateDevice(*honoClient, *tenant, deviceId)
-
-		if err == nil && resp.StatusCode != http.StatusNotFound {
-			//Get the registered device
-			device, _, _ = GetDevice(*honoClient, *tenant, deviceId)
-		}
-	} else if *inputDeviceId != 0 {
-		//Get the registered device
-		device, _, _ = GetDevice(*honoClient, *tenant, *inputDeviceId)
-	} else {
-		fmt.Println("Not a valid Device Id is provided to retrieve")
+	protocol := HTTP
+	if *p != 1 {
+		fmt.Println("The telemetry protocol is not supported")
+		os.Exit(1)
 	}
 
-	if *telemetry && device.DATA.ENABLED {
-		//send telemetry data
-		resp, err := SendTelemetry(*honoClient, *tenant, deviceId, GetRandomTemperature())
-		for err == nil && resp.StatusCode == http.StatusAccepted {
-			if *noDelay != true {
-				time.Sleep(time.Second * 1)
-			}
-			resp, err = SendTelemetry(*honoClient, *tenant, deviceId, GetRandomTemperature())
-		}
+	telemetryControl := &TelemetryControl{telemetry: *telemetry, noDelay: *noDelay, protocol: protocol}
 
+	httpClient := http.DefaultClient
+	honoClient := NewHonoRestClient(httpClient, *url)
+
+	deviceId := GetDeviceId(*inputDeviceId)
+	//register a new device
+	if *register {
+		for i := 0; i < *noOfClients; i++ {
+			go RegisterAndSendTelemetry(*honoClient, *tenant, deviceId, *telemetryControl)
+			deviceId = GetRandomDeviceId()
+		}
+		var input string
+		fmt.Scanln(&input)
+	} else if *inputDeviceId != 0 {
+		GetDeviceAndSendTelemetry(*honoClient, *tenant, *inputDeviceId, *telemetryControl)
+	} else {
+		fmt.Println("Not a valid Device Id is provided to retrieve")
 	}
 }
--- rest.go
+++ rest.go
@@ -7,6 +7,17 @@ import (
 	"strings"
 )
 
+type Honoc struct {
+}
+
+type PROTOCOL int
+
+const (
+	HTTP PROTOCOL = 1 + iota
+	MQTT
+	AMQP
+)
+
 //data structure representing a device data as returned by Hono Rest adapter
 type DATA struct {
 	ENABLED bool `json:enabled`
@@ -23,7 +34,7 @@ type DEVICE_REG_PARAMS struct {
 	DEVICE_ID int `url:"device_id"`
 }
 
-//A wrapper struct around the Sling http client
+//a wrapper struct around the Sling http client
 type HonoClient struct {
 	sling *s.Sling
 }
@@ -39,16 +50,16 @@ func NewHonoRestClient(httpClient *http.Client, baseUrl string) *HonoClient {
 func CreateDevice(h HonoClient, tenant string, deviceId int) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s", "registration", tenant)
 	deviceBody := fmt.Sprintf("device_id=%d", deviceId)
-	fmt.Println("Device registration data : ", deviceBody)
+	fmt.Printf("[%d] Device registration data : %s\n", deviceId, deviceBody)
 	body := strings.NewReader(deviceBody)
 	req, _ := h.sling.New().Post(path).Set("Content-Type", "application/x-www-form-urlencoded").Body(body).Request()
 
 	resp, err := h.sling.Do(req, nil, nil)
 
 	if err != nil {
-		fmt.Println("Register device Error: ", err)
+		fmt.Printf("[%d] Register device Error: %s\n", deviceId, err.Error())
 	} else {
-		fmt.Println("Register device Repsonse: ", resp.Status)
+		fmt.Println("[%d] Register device Repsonse: %s\n", deviceId, resp.Status)
 	}
 
 	return resp, err
@@ -62,27 +73,27 @@ func GetDevice(h HonoClient, tenant string, deviceId int) (*DEVICE, *http.Respon
 	resp, err := h.sling.Do(req, device, nil)
 
 	if err != nil {
-		fmt.Println("GET device Error: ", err.Error())
+		fmt.Printf("[%d] GET device Error: %s\n", deviceId, err.Error())
 	} else {
-		fmt.Println("GET device Response: ", resp.Status)
-		fmt.Printf("Retrieved device: DeviceId[%s] enabled[%t]\n", device.ID, device.DATA.ENABLED)
+		fmt.Printf("[%d] GET device Response: %s\n", deviceId, resp.Status)
+		fmt.Printf("[%d] Device enabled[%t]\n", deviceId, device.DATA.ENABLED)
 	}
 
 	return device, resp, err
 }
 
-//Sends the given telemetry data to the Hono's REST Adapter
+//sends the given telemetry data to the Hono's REST Adapter
 func SendTelemetry(h HonoClient, tenant string, deviceId int, data string) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s/%d", "telemetry", tenant, deviceId)
-	fmt.Println("Sending telemetry data : ", data)
+	fmt.Printf("[%d] Sending telemetry data : %s\n", deviceId, data)
 	body := strings.NewReader(data)
 	req, _ := h.sling.New().Put(path).Set("Content-Type", "application/json").Body(body).Request()
 
 	resp, err := h.sling.Do(req, nil, nil)
 	if err != nil {
-		fmt.Println("Telemetry Error: ", err)
+		fmt.Printf("[%d] Telemetry Error: %s\n", deviceId, err.Error())
 	} else {
-		fmt.Println("Telemetry Response: ", resp.StatusCode)
+		fmt.Printf("[%d] Telemetry Response: %d\n", deviceId, resp.StatusCode)
 	}
 	return resp, err
 }
